0412787bca23d45dadb95058541c3694503dcc20
Misa-Fumakiya <mishafumakiya@gmail.com>
2024-06-17 09:37:20 +0530

    End

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,22 +1,12 @@
-import { createContext, useContext, useReducer, useEffect } from "react";
+import { createContext, useContext, useReducer } from "react";
 import reducer from "../reducer/cartReducer";
 
 const CartContext = createContext();
 
-const getLocalCartData = () => {
-  let localCartData = localStorage.getItem("thapaCart");
-  if (localCartData === []) {
-    return [];
-  } else {
-    return JSON.parse(localCartData);
-  }
-};
-
 const initialState = {
-  // cart: [],
-  cart: getLocalCartData(),
+  cart: [],
   total_item: "",
-  total_price: "",
+  total_amount: "",
   shipping_fee: 50000,
 };
 
@@ -27,45 +17,12 @@ const CartProvider = ({ children }) => {
     dispatch({ type: "ADD_TO_CART", payload: { id, color, amount, product } });
   };
 
-  // increment and decrement the product
-
-  const setDecrease = (id) => {
-    dispatch({ type: "SET_DECREMENT", payload: id });
-  };
-
-  const setIncrement = (id) => {
-    dispatch({ type: "SET_INCREMENT", payload: id });
-  };
-
-  // to remove the individual item from cart
   const removeItem = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
 
-  // to clear the cart
-  const clearCart = () => {
-    dispatch({ type: "CLEAR_CART" });
-  };
-
-  // to add the data in localStorage
-  // get vs set
-
-  useEffect(() => {
-    dispatch({ type: "CART_TOTAL_ITEM" });
-    dispatch({ type: "CART_TOTAL_PRICE" });
-    localStorage.setItem("thapaCart", JSON.stringify(state.cart));
-  }, [state.cart]);
-
   return (
-    <CartContext.Provider
-      value={{
-        ...state,
-        addToCart,
-        removeItem,
-        clearCart,
-        setDecrease,
-        setIncrement,
-      }}>
+    <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
       {children}
     </CartContext.Provider>
   );
